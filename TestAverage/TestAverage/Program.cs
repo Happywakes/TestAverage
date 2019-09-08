2ea427b7512be8ae1bd07ccd3b0f0ea048acf742
John Clark <bennyclarkup@gmail.com>
2019-09-08 05:27:31 -0500

    Accounted for number of scores being zero

--- TestAverage/TestAverage/Program.cs
+++ TestAverage/TestAverage/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-
 namespace TestAverage
 {
     class Program
@@ -8,26 +7,26 @@ namespace TestAverage
         static void Main(string[] args)
         {
             Console.WriteLine("How many scores do you have?");
-            int ScoreNumber = int.Parse(Console.ReadLine());
 
-            int[] ScoreGroup = new int[ScoreNumber];
-
-            for (int i = 0; i < ScoreNumber; i++)
-            {
-                Console.WriteLine("What is score number " + (i + 1) + " ?");
-                ScoreGroup[i] = int.Parse(Console.ReadLine());
-            }
-
-            Console.WriteLine("Your avg is " + ScoreGroup.Average() + ".");
-
-           
             
+            int ScoreNumber = int.Parse(Console.ReadLine());
+            if(ScoreNumber == 0)
+            {
+                Console.WriteLine("Blame Billy as he never scores.");
+            }
+            else
+            {
+                int[] ScoreGroup = new int[ScoreNumber];
+
+                for (int i = 0; i < ScoreNumber; i++)
+                {
+                    Console.WriteLine("What is score number " + (i + 1) + " ?");
+                    ScoreGroup[i] = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("Your avg is " + ScoreGroup.Average() + ".");
 
 
-
-
-
-
-        }
+            }
+        }    
     }
 }
